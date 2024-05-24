208b847ec9df07480316f1aa95232d79b668e38a
Kh Raouf <raoufg716@gmail.com>
2024-05-24 01:32:30 +0100

    test 3 commit

--- app/(dashboard)/(routes)/teacher/students/[studentId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/students/[studentId]/page.tsx
@@ -1,11 +1,10 @@
 import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
-import React from 'react'
 import UserProfile from './_components/UserProfile';
 interface PageProps {
   params: { studentId: string };
 }
-export const page = ({params}:PageProps) => {
+export const StudentsPage = async ({params}:PageProps) => {
     const { userId } = auth();
   if (!userId) {
     return redirect("/");
