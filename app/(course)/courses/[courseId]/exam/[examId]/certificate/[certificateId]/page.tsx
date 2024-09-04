e4af4e164e88628bec43e32c154973a8430a6129
raoufkh124 <fvraouf716@gmail.com>
2024-09-04 15:01:22 +0000

    edit certificate

--- app/(course)/courses/[courseId]/exam/[examId]/certificate/[certificateId]/page.tsx
+++ app/(course)/courses/[courseId]/exam/[examId]/certificate/[certificateId]/page.tsx
@@ -8,14 +8,24 @@ import { useAuth } from "@clerk/nextjs";
 import { redirect, useRouter } from "next/navigation";
 import axios from "axios";
 import { Certificate } from "@prisma/client";
+import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
+import { Document, Page, pdfjs } from 'react-pdf';
+import fontkit from '@pdf-lib/fontkit'
+
+// Text layer for React-PDF.
+import 'react-pdf/dist/Page/TextLayer.css';
 
 const CertificatePage = ({
   params,
 }: {
   params: { courseId: string; examId: string; certificateId: string };
 }) => {
+  pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.mjs',
+    import.meta.url,
+  ).toString();
   const htmlRef = useRef<HTMLDivElement>(null);
-
+  const [certificatePdf, setCertificatePdf] = useState<string>('')
   const { userId } = useAuth();
   const router = useRouter()
   const [certificate, setCertificate] = useState<Certificate>();
@@ -25,12 +35,50 @@ const CertificatePage = ({
   useEffect(() => {
     (async () => {
       setisGettingCertificate(true);
-      
+      const url = 'https://uz635cwohid0t4ce.public.blob.vercel-storage.com/Appreciation%20Certificate%20%D8%B4%D9%87%D8%A7%D8%AF%D8%A9%20%D8%AA%D9%82%D8%AF%D9%8A%D8%B1%20%D8%A3%D8%B2%D8%B1%D9%82%20%D9%88%D8%A3%D8%AE%D8%B6%D8%B1%20(2)-4GF2H0oU8fk7pv6dP4xuqGw4mVJUJE.pdf'
+      const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
+      const pdfDoc = await PDFDocument.load(existingPdfBytes)
+      const pages = pdfDoc.getPages()
+      const firstPage = pages[0]
+      console.log(existingPdfBytes)
+
       try {
         const response = await axios.get(
           `/api/courses/${params.courseId}/exam//${params.examId}/certificate/${params.certificateId}`
         );
+        const fontUrl = 'https://uz635cwohid0t4ce.public.blob.vercel-storage.com/Cairo-Regular-IfPEScEah1exU4X87UiQ3sPhWk8hQ6.ttf'
+        pdfDoc.registerFontkit(fontkit)
+        const fontBytes = await fetch(fontUrl).then(res => res.arrayBuffer())
+        const customFont = await pdfDoc.embedFont(fontBytes)
+        const textSize = 35
+        const date = new Date(response.data.createdAt)
+        firstPage.drawText(response.data.nameOfStudent, {
+          x:  400,
+          y: 285,
+          size: textSize,
+          font: customFont,
+          color: rgb(0, 0.53, 0.71),
+        })
+        firstPage.drawText(response.data.courseTitle, {
+          x:  400,
+          y: 150,
+          size: textSize,
+          font: customFont,
+          color: rgb(0, 0.53, 0.71),
+        })
+        firstPage.drawText(`${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`, {
+          x:  150,
+          y: 72,
+          size: 20,
+          font: customFont,
+          color: rgb(0, 0.53, 0.71),
+        })
 
+        const pdfBytes = await pdfDoc.save()
+
+        var b64encoded = Buffer.from(pdfBytes).toString('base64');
+        setCertificatePdf(`data:application/pdf;base64,` + b64encoded)
+        console.log(response.data)
         setCertificate(response.data);
 
         if (!response.data) {
@@ -79,7 +127,7 @@ const CertificatePage = ({
       {isGettingCertificate ? (
         <div className="flex items-center justify-center h-full w-full">
           <div className="font-bold text-2xl text-slate-500 animate-pulse">
-           ...تحضير شهادتك
+            ...تحضير شهادتك
           </div>
         </div>
       ) : certificate ? (
@@ -90,67 +138,15 @@ const CertificatePage = ({
           >
             <div> تحميل PDF</div>
           </button>
+
           <div
             ref={htmlRef}
             id="certificate"
-            className="w-full h-[450px] shadow-lg flex flex-row-reverse text-right"
+            className="w-full overflow-hidden h-[700px] shadow-lg flex flex-row-reverse text-right"
           >
-            <div className="h-full flex-[.9] flex flex-col items-end justify-center px-12 pt-2 pb-12 mb-12 space-y-4">
-              <div className="flex -space-x-3">
-                <div className="font-extrabold text-sky-700 text-5xl italic">ل</div>
-                <div className="font-extrabold text-green-700 text-5xl">م</div>
-                <div className="font-extrabold text-sky-300 text-5xl italic">س</div>
-              </div>
-
-              <div>
-                <div className="text-4xl text-sky-700 font-bold">
-                شهادة الانتهاء
-                </div>
-                <div className="text-2xl text-sky-700 font-light">
-                تهانينا،
-                </div>
-              </div>
-              <div>
-                <div className="text-3xl text-sky-700 font-bold">
-                  {certificate?.nameOfStudent}
-                </div>
-                <div className="text-xl text-sky-700">
-                تم إكماله بنجاح
-                </div>
-              </div>
-              <div>
-                <div className="text-3xl text-sky-700 font-bold">
-                  {certificate?.courseTitle}
-                </div>
-                <div className="text-xl text-sky-700">
-                اكتمل على{" "}
-                  <span className="font-semibold">
-                    {" "}
-                    {certificate?.dateOfIssuance.toLocaleString().split("T")[0]}
-                  </span>
-                </div>
-              </div>
-              <div className="-space-y-2">
-                <div className="text-base text-sky-700">
-                بإكمال هذه الدورة تكون قد أضفت إليك المعرفة والخبرة
-                  مهارات،
-                </div>
-                <div className="text-base text-sky-700">
-                وخلقت لك فرصًا جديدة
-                </div>
-              </div>
-            </div>
-            <div className="min-h-full h-full bg-sky-700 w-40 flex justify-center border-r-8 border-l-8 border-green-700">
-              <div className="bg-sky-700 h-full w-full relative">
-                <Image
-                  src="/badge.jpg"
-                  alt="badge"
-                  width={180}
-                  height={180}
-                  className="aspect-auto mt-8 h-fit rounded-full absolute top-10"
-                />
-              </div>
-            </div>
+            <Document file={certificatePdf} >
+              <Page height={400} width={1000} pageNumber={1} />
+            </Document>
           </div>
         </div>
       ) : null}
